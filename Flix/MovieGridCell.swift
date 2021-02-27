16710283df52fba10d0ae1d86e09f3eef3724427
Richard Ngo-Lam <rngolam@gmail.com>
2021-02-27 00:15:43 -0600

    Added navigation functionality, movie detail view, and a collection view for Superhero movies.

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Richie Ngo-Lam on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
